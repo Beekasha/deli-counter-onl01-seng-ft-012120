3547862134be430287376490ac08acb3d70740b9
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-23 01:22:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli=[])
+numbered_people = []
+  
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index do |person, idx|
+      
+      numbered_people << " #{idx + 1}. #{person}"
+
+    end
+    
+    puts "The line is currently:#{numbered_people.join("")}"
+    
+  end
+end
